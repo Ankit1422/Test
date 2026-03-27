52a5608167de13d42adea24af7a340b8bf849e00
Ankit <ku_ank@live.concordia>
2026-03-27 13:22:58 -0400

    first commit

---
+++ add.py
@@ -0,0 +1,11 @@
+# This program adds two numbers
+
+# store input numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# add two numbers
+total = num1 + num2
+
+# display the sum
+print("The sum of", num1, "and", num2, "is", total)
